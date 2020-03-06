b90f8d9e73a420ddafc326ddaee69005e1abc80f
elgill <cgillin2@gmail.com>
2020-03-06 18:53:02 -0500

    Removed Unnecary file, changed what is logged, and made it every 60 seconds instead of when a button is pressed

--- tempLog.py
+++ tempLog.py
@@ -5,9 +5,10 @@ import time
 import os
 #Assign GPIO pins
 redPin = 27
+greenPin = 26
 tempPin = 17
 
-buttonPin = 19
+#buttonPin = 19
 
 #Temp and Humidity Sensor
 tempSensor = Adafruit_DHT.DHT11
@@ -20,7 +21,8 @@ blinkTime = 7
 #Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
-GPIO.setup(buttonPin, GPIO.IN)
+GPIO.setup(greenPin,GPIO.OUT)
+#GPIO.setup(buttonPin, GPIO.IN)
 
 def oneBlink(pin):
 	GPIO.output(pin,True)
@@ -37,20 +39,31 @@ def readF(tempPin):
 		print('Error Reading Sensor')
 	return tempFahr
 
+def readHum(tempPin):
+	humidity, temperature = Adafruit_DHT.read_retry(tempSensor,tempPin)
+	if humidity is not None and temperature is not None:
+		hum = '{0:0.1f}%'.format(humidity)
+	else:
+		print('Error Reading Sensor')
+	return hum
+
 try:
 	with open("./log/tempLog.csv", "a") as log:
-
+		start=time.time()
 		while True:
-			input_state = GPIO.input(buttonPin)
-			if input_state == True:
+#			input_state = GPIO.input(buttonPin)
+			if True:
 				for i in range (blinkTime):
 					oneBlink(redPin)
 				time.sleep(.2)
-				data = readF(tempPin)
-				print (data)
-				log.write("{0},{1}\n".format(time.strftime("%Y‐%m‐%d %H:%M:%S"),str(data)))
+				temp = readF(tempPin)
+				hum = readHum(tempPin)
+				print("Temperature: ",str(temp),str(hum))
+				log.write("{0},{1},{2}\n".format(time.strftime("%Y‐%m‐%d %H:%M:%S"),str(temp),str(hum)))
 				log.flush()
 				os.fsync(log)
+				#adjusts to make it actually 60 seconds
+				time.sleep(60-((time.time() - start) % 60))
 except KeyboardInterrupt:
 	os.system('clear')
 	print('Thanks for Blinking and Thinking!')
